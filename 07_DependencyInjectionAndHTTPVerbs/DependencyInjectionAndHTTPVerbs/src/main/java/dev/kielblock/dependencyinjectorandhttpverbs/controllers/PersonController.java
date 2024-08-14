0dd2990c952d4c9b33216ee7e81d781c8d77d43e
Pedro Kielblock <pedro@kielblock.dev>
2024-08-14 11:38:31 -0300

    fix: annotation @repository is no longer necessary when extending jpa repository

--- 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/controllers/PersonController.java
+++ 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/controllers/PersonController.java
@@ -4,6 +4,7 @@ import dev.kielblock.dependencyinjectorandhttpverbs.model.Person;
 import dev.kielblock.dependencyinjectorandhttpverbs.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,32 +16,33 @@ public class PersonController {
     @Autowired
     private PersonService service;
 
-    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
 
         return service.findAll();
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id) {
 
         return service.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) {
 
         return service.create(person);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@RequestBody Person person) {
 
         return service.update(person);
     }
 
-    @RequestMapping(value =("/{id}"), method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id) {
+    @DeleteMapping(value =("/{id}"))
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
--- 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/repositories/PersonRepository.java
+++ 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/repositories/PersonRepository.java
@@ -2,8 +2,6 @@ package dev.kielblock.dependencyinjectorandhttpverbs.repositories;
 
 import dev.kielblock.dependencyinjectorandhttpverbs.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 }

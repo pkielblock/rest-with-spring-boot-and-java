4dfd4cc148f6315d79d56ae125f57c554bd6f809
Pedro Kielblock <pedro@kielblock.dev>
2024-08-14 11:56:03 -0300

    fix: fix warnings on log

--- 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/model/Person.java
+++ 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/model/Person.java
@@ -15,10 +15,13 @@ public class Person implements Serializable {
 
     @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
+
     @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
+
     @Column(name = "address", nullable = false, length = 100)
     private String address;
+
     @Column(name = "gender", nullable = false, length = 6)
     private String gender;
 
--- 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/services/PersonService.java
+++ 07_DependencyInjectionAndHTTPVerbs/DependencyInjectionAndHTTPVerbs/src/main/java/dev/kielblock/dependencyinjectorandhttpverbs/services/PersonService.java
@@ -6,9 +6,7 @@ import dev.kielblock.dependencyinjectorandhttpverbs.repositories.PersonRepositor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 @Service
